bcfe3e1e18f3b152a115d0e4df39dbb5d9befd8a
Araz Abishov <arabisho@microsoft.com>
2025-12-23 16:06:50 +0400

    wip: porting more components from the theme

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -6,3 +6,20 @@ export const Site = {
   description: "Bits and bytes about open source, Android and Rust.",
   locale: "en-us",
 };
+
+export const NAV_LINKS = [
+  { label: "Blog", href: "/" },
+  { label: "About", href: "/about" },
+];
+
+export const SOCIAL_LINKS = [
+  { label: "GitHub", href: "https://github.com/arazabishov" },
+  { label: "Twitter", href: "https://twitter.com/arazabishov" },
+  { label: "RSS", href: "/rss.xml" },
+];
+
+export const ICON_MAP = {
+  GitHub: "lucide:github",
+  Twitter: "lucide:twitter",
+  RSS: "lucide:rss",
+};
