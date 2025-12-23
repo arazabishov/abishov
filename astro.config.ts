89d75f016b97cd7b4dc030d075d991404a759d96
Araz Abishov <arabisho@microsoft.com>
2025-12-23 12:39:44 +0400

    wip: bootstrapping dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://abishov.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
