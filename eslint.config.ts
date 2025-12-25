a35484e25630bc036fe8df46ea8d1cd5c558b1d0
Araz Abishov <arabisho@microsoft.com>
2025-12-25 12:02:33 +0400

    wip: eslint integration

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist/", ".astro/", "node_modules/"],
+  },
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["src/env.d.ts"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+];
