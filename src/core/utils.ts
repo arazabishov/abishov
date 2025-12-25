4489b54af477383c71e8be01fe171f0eceb4bd7c
Araz Abishov <arabisho@microsoft.com>
2025-12-25 16:39:30 +0400

    wip: starting to change the projects layout

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,15 +1,13 @@
 import type { CollectionEntry } from "astro:content";
 
-function calculateWordCountFromHtml(html: string | null | undefined): number {
-  if (!html) {
-    return 0;
-  }
-  const textOnly = html.replace(/<[^>]+>/g, "");
-  return textOnly.split(/\s+/).filter(Boolean).length;
-}
-
 export function getPostReadingTime(post: CollectionEntry<"blog">): string {
-  const wordCount = calculateWordCountFromHtml(post.body);
+  const html = post.body;
+  const wordCount = html
+    ? html
+        .replace(/<[^>]+>/g, "")
+        .split(/\s+/)
+        .filter(Boolean).length
+    : 0;
   const readingTimeMinutes = Math.max(1, Math.round(wordCount / 200));
   return `${readingTimeMinutes} min read`;
 }
